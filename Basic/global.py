b3fac0c7e92bbe171a7f057e0f757aff70216fb5
yeonjuko <yjoogo@gmail.com>
2023-03-09 22:42:16 +0900

    함수,람다식

---
+++ Basic/global.py
@@ -0,0 +1,10 @@
+a = 0
+
+def func():
+    global a
+    a += 1
+
+for i in range(10):
+    func()
+
+print(a)
---
+++ Basic/lambdaEx.py
@@ -0,0 +1,7 @@
+array = [('홍길동', 50), ('이순신', 32), ('아무개', 74)]
+
+def my_key(x):
+    return x[1]
+
+print(sorted(array, key=my_key))
+print(sorted(array, key=lambda x: x[1]))
